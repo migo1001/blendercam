8caf8ae4c9251277a53c86f5636bbf78faacd43e
migo101 <migo101@gmx.fr>
2022-04-06 13:24:57 +0200

    Tests - not working so far

--- scripts/addons/cam/tests/test_info_panel.py
+++ scripts/addons/cam/tests/test_info_panel.py
@@ -10,10 +10,8 @@ class TestInfoPanel(unittest.TestCase):
     def setUpClass(cls):
         for panel in bpy.types.Panel.__subclasses__():
             if panel.__name__ == "CAM_INFO_Panel":
-                cls.info_panel = panel
+                cls.info_panel = panel()
                 continue
-
-
 
     def test_addon_enabled(self):
         info_panel = None
@@ -21,10 +19,10 @@ class TestInfoPanel(unittest.TestCase):
         self.assertEqual("CAM info & warnings", self.info_panel.bl_label)
 
     def test_warnings(self):
-        self.assertEqual("CAM info & warnings", self.info_panel.draw_active_op_warnings(self.info_panel))
+        self.assertEqual("CAM info & warnings", self.info_panel.draw_active_op_warnings())
 
     def test_opencamlib(self):
-        self.assertEqual("Opencamlib vxxxx installed", TestInfoPanel.info_panel.draw_opencamlib_version(TestInfoPanel.info_panel))
+        self.assertEqual("Opencamlib vxxxx installed", TestInfoPanel.info_panel.draw_opencamlib_version())
 
 
 
@@ -33,7 +31,5 @@ class TestInfoPanel(unittest.TestCase):
         bpy.context.scene.cam_machine.hourly_rate = 200.0
         self.assertEqual(bpy.context.scene.cam_machine.hourly_rate, 200.0)
 
-#Créer un ensemble de test
 suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestInfoPanel)
-#Essai
 unittest.TextTestRunner().run(suite)
